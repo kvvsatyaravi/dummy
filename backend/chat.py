3756432d090174b563143fc69dd12964275f4639
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2021-07-11 07:50:11 +0530

    testing failed

--- backend/chat.py
+++ backend/chat.py
@@ -3,15 +3,15 @@ import json
 
 import torch
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from backend.Model import NeuralNet
+from backend.nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('backend/intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
+FILE = "backend/data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
@@ -27,27 +27,31 @@ model.eval()
 
 bot_name = "Sam"
 print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
+def chat():
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
 
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
 
-    tag = tags[predicted.item()]
+        tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    botvalue = f"{bot_name}: {random.choice(intent['responses'])}"
+        else:
+            botvalue = f"{bot_name}: I do not understand..."
+
+        return botvalue
+    
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from backend.chat import  bot_name,chat
 
 
 class mainpage(TemplateView):
@@ -7,3 +8,9 @@ class mainpage(TemplateView):
 
 	def get(self,request):
 		return render(request,self.Template_view)
+
+	def post(self,request):
+		if request.method == 'POST':
+			context={"bot":chat()}
+			
+		return render(request,self.Template_view,context)
